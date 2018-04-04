c5ffe3a9b094c28eb41b83074e4a73d2d9fce1f3
EtherealArd <vaultec114@gmail.com>
2018-04-04 18:30:34 +0300

    user signup and validation

--- app.js
+++ app.js
@@ -6,29 +6,36 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var expressHbs = require('express-handlebars')
 var mongoose = require('mongoose');
-
+var session = require('express-session');
+var passport = require('passport');
+var flash = require('connect-flash');
+var validator = require('express-validator');
 var index = require('./routes/index');
 
 var app = express();
 
 mongoose.connect('mongodb://localhost:27017/shopping');
+require('./config/passport');
 
-// view engine setup
 app.engine('.hbs',expressHbs({defaultLayout: 'layout', extname: '.hbs'}));
 app.set('view engine', '.hbs');
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(validator());
 app.use(cookieParser());
+app.use(session({secret:'mysupersecret', resave: false, saveUnitialized:false}));
+app.use(flash());
+app.use(passport.initialize());
+app.use(passport.session());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
 
 
-// catch 404 and forward to error handler
+
 app.use(function(req, res, next) {
   var err = new Error('Not Found');
   err.status = 404;
--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,11 @@
 var express = require('express');
 var router = express.Router();
+var csrf = require('csurf');
+var passport = require('passport');
 var Product = require('../models/product');
-/* GET home page. */
+var csrfProtection = csrf();
+router.use(csrfProtection);
+
 router.get('/', function(req, res, next) {
 
   Product.find(function(arr,docs){
@@ -15,6 +19,20 @@ router.get('/', function(req, res, next) {
     res.render('shop/index', { title: 'Express', products:productChunks});
   });
   
+});
+router.get('/user/signup', function(req,res,next){
+ var messages = req.flash('error');
+  res.render('user/signup',{csrfToken: req.csrfToken(), messages: messages, hasErrors: messages.length>0});
+});
+
+router.post('/user/signup', passport.authenticate('local.signup', {
+  successRedirect: '/user/profile',
+  failureRedirect: '/user/signup',
+  failureFlash: true
+}));
+
+router.get('/user/profile', function(req, res, next){
+  res.render('user/profile');
 });
 
 module.exports = router;
